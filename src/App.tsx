84e648a3f7e48ee158a5ba21b54d592a6738b33f
wayfind26 <kaela@letswayfind.com>
2026-04-22 08:38:19 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { StatusBar } from "expo-status-bar";
 export default function App() {
   return (
     
-      Wayfind - The Planner's Advantage
+      Wayfind. The Planner's Advantage.
       
-        Planner-centric community for discovering vetted vendors, sharing reviews, and collaborating with peers.
+        Planner centered community for discovering vetted vendors, sharing reviews, and collaborating with peers.
       
       
     
